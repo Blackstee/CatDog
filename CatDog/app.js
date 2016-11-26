d365602f356f76b8b47b58c0c1088d3e9462bd60
Blackstee <ya.nastuha24@gmail.com>
2016-11-26 02:44:24 +0200

    works

--- CatDog/app.js
+++ CatDog/app.js
@@ -1,7 +1,9 @@
 "use strict";
 
 const express = require('express');
+var logger = require('morgan');
 const bodyParser = require('body-parser');
+var mongoose = require ('mongoose');
 const busboyBodyParser = require('busboy-body-parser');
 const mongodb = require('promised-mongo');
 const crypto = require('crypto');
@@ -14,90 +16,21 @@ const app = express();
 var routes = require('./routes/index');
 var users = require('./routes/users');
 
-
-var MongoClient = require('mongodb').MongoClient, format = require('util').format;
 var path = require('path');
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
-
+app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(busboyBodyParser({ limit: '5mb' }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(session({ secret: 'keyboard cat', cookie: { maxAge: 60000 }, resave: true, saveUninitialized: true}))
-app.use(passport.initialize());
-app.use(passport.session());
+app.use(session({ secret: 'keyboard cat', cookie: { maxAge: 60000 }, resave: true, saveUninitialized: true}));
+//app.use(passport.initialize());
+//app.use(passport.session());
 app.use('/', routes);
 app.use('/users', users);
-/*
-MongoClient.connect('mongodb://127.0.0.1:27017/test', function(err, db) {
-  if(err) throw err;
-    var users = db.collection('catdogusers');
-
-users.insert({ nickname: "autor", password: "autor"}, function(err, docs) {
-
-users.count(function(err, count) {
-console.log(format("count = %s", count));
-});
-});
-
-users.find().toArray(function(err, results) {
-console.dir(results);
-
-db.close();
-});
-});*/
-
-const url = 'mongodb://localhost:27017/test';
-const db = mongodb(url);
-
-let sessionSecret = "jahdgalsdg^&(*&^%  _Asds)";
-
-
-passport.serializeUser(function(user, done) {
-    done(null, user._id);
-});
-
-passport.deserializeUser(function(id, done) {
-	console.log("deserializeUser id: " + id);
-	db.users.findOne({ _id: mongodb.ObjectId(id) })
-		.then(user => {
-			if(user) {
-				done(null, user);
-			} else {
-				done("No user", null);
-			}
-		})
-		.catch(err => done(err, null));
-});
-
-passport.use(new LocalStrategy((username, password, done) => {
-	  console.log("Local: " + username + " : " + password);
-	  db.users.findOne({
-			  username: username,
-			  passwordHash: hash(password)
-		  })
-		  	.then(user => {
-				console.log(user);
-				if (user) {
-					done(null, user);
-				} else {
-					done(null, false);
-				}
-			})
-			.catch(err => {
-				console.log(err);
-				done(err, null);
-			});
-}));
-
-const salt = 'sid_fhKJG-=-*&8734';
-
-function hash(pass) {
-	return crypto.createHash('md5').update(pass + salt).digest("hex");
-}
-
+mongoose.connect ('mongodb://localhost/myapp');
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -130,67 +63,7 @@ app.use(function(err, req, res, next) {
   });
 });
 
-/*var mongoose = require('mongoose'),
-    User = require('./user-model');
 
-var connStr = 'mongodb://localhost:27017/mongoose-bcrypt-test';
-mongoose.connect(connStr, function(err) {
-    if (err) throw err;
-    console.log('Successfully connected to MongoDB');
-});
-/*
-// create a user a new user
-var testUser = new User({
-    username: 'jmar2',
-    password: 'Password2'
-});
-
-// save user to database
-testUser.save(function(err) {
-    if (err) throw err;
-    User.getAuthenticated('jmar2', 'Password3', function(err, user, reason) {
-        if (err) throw err;
-        //success
-        if (user) {
-            console.log('login success');
-            return;
-        }
-
-        // otherwise we can determine why we failed
-        var reasons = User.failedLogin;
-        switch (reason) {
-            case reasons.NOT_FOUND:
-            case reasons.PASSWORD_INCORRECT:
-                console.log ('failed! Try again');
-                break;
-            case reasons.MAX_ATTEMPTS:
-                console.log ('too much attempts');
-                break;
-        }
-    });
-});
-
-*/
-app.post('/register', (req, res) => {
-	let username = req.body.username;
-	let pass = req.body.pass;
-	let pass2 = req.body.pass2;
-	if (!username || !pass || (pass !== pass2)) res.status(400).end('not ok');
-	else {
-		db.User.findOne({ username: username})
-			.then(User => {
-				if (User) res.status(200).end('user exists');
-				else {
-					return db.sers.insert({
-						username: username,
-						password: crypto.createHash('md5').update(pass + salt).digest("hex")
-					});
-				}
-			})
-			.then(() => res.redirect('/profile'))
-			.catch(err => res.status(500).end(err));
-	}
-});
 
 
 module.exports = app;
--- CatDog/routes/index.js
+++ CatDog/routes/index.js
@@ -1,42 +1,71 @@
-"use strict"
+"use strict";
 
 const passport = require('passport');
 const LocalStrategy = require('passport-local').Strategy;
 var express = require('express');
 var router = express.Router();
-const mongodb = require('promised-mongo');
-const url = 'mongodb://localhost:27017/test';
-const db = mongodb(url);
+var User = require ('./user');
 /* GET home page. */
 router.get('/', function(req, res, next) {
 	const user = req.session.user;
 	res.render('index', { user: user });
 });
 
-router.get('/register', (req, res) => res.render("users_index", { user: req.user }));
+router.get('/register', (req, res) => res.render("register", { user: req.user }));
+
+router.get('/tologin', (req, res) => res.render ("login"));
 
 router.get('/login', (req, res) => res.render("users_login"));
 
 router.get('/logout', (req, res) => {
-	req.logout();
+	req.session.destroy();
 	res.redirect('/');
+
+	return res.status(200).send();
 });
 
 router.get('/register-error', (req, res) => res.end("Register error"));
 
-router.post('/login',
-	passport.authenticate('local', { failureRedirect: '/' }),
-	(req, res) => res.redirect('/'));
+router.post('/login', function (req, res){
+  var username = req.body.username;
+  var password = req.body.password;
+
+  User.findOne({username: username}, function (err, user){
+    if (err){
+      console.log(err);
+      return res.status(500).send();
+    }
+    if (!user) {
+      return res.status(404).send();
+    }
+    user.comparePassword(password, function(err, isMatch) {
+        if (isMatch && isMatch == true){
+          req.session.user = user;
+          return res.status(200).send();
+        } else {
+          return res.status(401).send();
+        }
+        res.redirect('/');
+    });
+  })
+});
+
+router.get('/dashboard', function(req, res){
+  if (!req.session.user){
+    return res.status(401).send();
+  }
+  return res.status(200).send("Welcooome");
+})
 
 router.post('/upload_avatar',
 	(req, res) => {
 		let avaObj = req.files.avatar;
 		let username = req.user.username;
 		let base64String = avaObj.data.toString('base64');
-		db.users.findOne({ username: username})
+		User.findOne({ username: username})
 			.then(user => {
 				if (user) {
-					return db.users.findAndModify({
+					return User.findAndModify({
 					    query: { username: username },
 					    update: { $set: { avatar: base64String } },
 					    new: false
@@ -50,31 +79,37 @@ router.post('/upload_avatar',
 			.catch(err => res.status(500).end(err));
 	});
 
-router.post('/register',
-	(req, res) => {
-		let username = req.body.username;
-		let pass = req.body.pass;
-		let pass2 = req.body.pass2;
-		console.log(username);
-		if (!username || !pass || pass !== pass2) {
+router.post('/register', function(req, res){
+  var username = req.body.username;
+  var password = req.body.pass;
+  var password2 = req.body.pass2;
+  var firstname = req.body.firstname;
+  var lastname = req.body.lastname;
+  console.log(username);
+		if (!username || !password || password !== password2) {
 			res.redirect('/register-error');
 		}
-		db.users.findOne({ username: username})
+  User.findOne({ username: username})
 			.then(x => {
 				if (x) {
 					res.end("Such username exists " + JSON.stringify(x));
 				} else {
-					db.insert({
-						username: username,
-						passwordHash: hash(pass)
-					})
-						.then(() => res.redirect('/login'))
-						.catch(err => res.status(500).end(err));
-				}
-			})
-			.catch(err => res.status(500).end(err));
-	});
+  var newuser = new User ();
+  newuser.username = username;
+  newuser.password = password;
+  newuser.firstname = firstname;
+  newuser.lastname = lastname;
+  newuser.save(function(err, savedUser){
+    if (err){
+      console.log(err);
+      return res.status(500).send();
+    }
+    return res.status(200).send();
+  })
+}
+})
+})
 
-router.get('/users', (req, res)=> db.users.find().then(users => res.json(users)));
+router.get('/users', (req, res)=> User.find().then(users => res.json(users)));
 
 module.exports = router;
